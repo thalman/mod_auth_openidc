95baad3342aa7ef7172ad4922eb9f0d605dc469b
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2018-12-06 14:55:44 +0100

    optionally delete the oldest state cookie(s); see #399
    bump to 2.3.10rc3
    
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>
    (cherry picked from commit 46758a75eef8e3c91f2917fc7c6302136eb18809)

--- src/parse.h
+++ src/parse.h
@@ -117,7 +117,7 @@ const char *oidc_parse_info_hook_data(apr_pool_t *pool, const char *arg, apr_has
 const char *oidc_parse_token_binding_policy(apr_pool_t *pool, const char *arg, int *int_value);
 const char *oidc_token_binding_policy2str(apr_pool_t *pool, int v);
 const char *oidc_parse_auth_request_method(apr_pool_t *pool, const char *arg, int *method);
-const char *oidc_parse_max_number_of_state_cookies(apr_pool_t *pool, const char *arg, int *int_value);
+const char *oidc_parse_max_number_of_state_cookies(apr_pool_t *pool, const char *arg1, const char *arg2, int *int_value, int *bool_value);
 
 typedef const char *(*oidc_valid_int_function_t)(apr_pool_t *, int);
 typedef const char *(*oidc_valid_function_t)(apr_pool_t *, const char *);
